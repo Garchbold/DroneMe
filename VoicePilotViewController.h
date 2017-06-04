62c18e82c322e6193b93b987e88bf4d1ae8e363e
Garchbold <garchbold@g.ucla.edu>
2017-06-04 15:18:57 -0700

    added first person camera view to app

---
+++ VoicePilotViewController.h
@@ -0,0 +1,18 @@
+//
+//  VoicePilotViewController.h
+//  DJISDKSwiftDemo
+//
+//  Created by George Archbold on 6/4/17.
+//  Copyright © 2017 DJI. All rights reserved.
+//
+
+#ifndef VoicePilotViewController_h
+#define VoicePilotViewController_h
+
+#import <UIKit/UIKit.h>
+
+@interface VoicePilotViewController : UIViewController
+
+@end
+
+#endif /* VoicePilotViewController_h */
